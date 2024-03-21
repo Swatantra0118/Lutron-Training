2166bbd4cc06212d23cd44ef79228028d3076f26
tannuverma <tannu.verma@nagarro.com>
2024-03-21 11:23:13 +0530

    WPF app comsuming OrderService

---
+++ LutronTrainingProject/LutronOrderingSystem/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Caliburn.Micro;
+using LutronOrderingSystem.Helpers;
+using LutronOrderingSystem.Models;
+using System.Windows.Input;
+
+namespace LutronOrderingSystem.ViewModels
+{
+    public class CartItemViewModel : PropertyChangedBase
+    {
+        private ProductModel _product;
+        public ProductModel Product
+        {
+            get { return _product; }
+            set
+            {
+                _product = value;
+                NotifyOfPropertyChange(() => Product);
+            }
+        }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                NotifyOfPropertyChange(() => Quantity);
+            }
+        }
+
+        public CartItemViewModel(ProductModel product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+
+        }
+
+        
+    }
+
+}
---
+++ LutronTrainingProject/LutronOrderingSystem/ViewModels/CartWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Caliburn.Micro;
+using LutronOrderingSystem.DataAccess;
+using LutronOrderingSystem.Helpers;
+using LutronOrderingSystem.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LutronOrderingSystem.ViewModels
+{
+    public class CartWindowViewModel : Screen
+    {
+        private BindableCollection<CartItemViewModel> _cartItems;
+        public BindableCollection<CartItemViewModel> CartItems
+        {
+            get { return _cartItems; }
+            set
+            {
+                _cartItems = value;
+                NotifyOfPropertyChange(() => CartItems);
+            }
+        }
+        private readonly DatabaseManager databasemanager;
+        public ICommand CheckoutCommand { get; private set; }
+        public ICommand RemoveCommand { get; private set; }
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl = "https://localhost:44391/api/Order";
+
+
+        public CartWindowViewModel(BindableCollection<CartItemViewModel> cartItems)
+        {
+            CartItems = cartItems;
+            RemoveCommand = new RelayCommand(removeItem);
+            CheckoutCommand = new RelayCommand(Checkout);
+            databasemanager = new DatabaseManager();
+            _httpClient = new HttpClient();
+
+        }
+        
+
+        public async void PlaceOrderAsync(List<CartItemDTO> cartItems)
+        {
+            try
+            {
+                //Serialize list to JSON
+                string cartItemsJson = JsonConvert.SerializeObject(cartItems);
+                var content = new StringContent(cartItemsJson, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync(_apiBaseUrl, content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error placing order: {ex.Message}");
+            }
+        }
+        private void removeItem(object obj)
+        {
+            if(obj is CartItemViewModel c)
+            {
+                CartItems.Remove(c);
+            }
+        }
+
+        private void Checkout(object obj)
+        {
+            if (CartItems.Count != 0)
+            {
+                MessageBox.Show("Your order is successfully placed !!", "Order Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Your cart is empty !!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            var Itemz= new List<CartItemDTO>();
+            foreach (var c in CartItems)
+            {
+                var newItem = new CartItemDTO();
+                newItem.Product = c.Product;
+                newItem.Quantity = c.Quantity;
+                Itemz.Add(newItem);
+                    ProductModel p = databasemanager.GetProductById(c.Product.ModelId);
+                    p.Quantity -= c.Quantity;
+                    databasemanager.UpdateProduct(p);
+            }
+            PlaceOrderAsync(Itemz);
+            CartItems.Clear();           
+        }
+    }
+    
+}
